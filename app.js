0190a7e7b2b9922ea80a930357806bf0fb31a741
Julie Ho <julieho.98@gmail.com>
2018-07-30 23:03:53 -0700

    add spotify api

--- app.js
+++ app.js
@@ -2,21 +2,6 @@ const authEndpoint = 'https://accounts.spotify.com/api/token';
 const clientID = 'c180770a2f4144078103e268866ea767';
 const redirectURI = 'http://localhost:8888';
 
-function requestAuth(clientID) {
-	const settings = {
-		url: authEndpoint,
-		data: {
-			client_id: clientID,
-			response_type: 'token',
-			redirect_uri: redirectURI
-		},
-		type: 'POST',
-		dataType: 'json',
-		success: function(response) {console.log(response);}
-	}
-	$.ajax(settings);
-}
-
 $('.album-accordion').on('click', '.album', function(event) {
 	$(this).next('.tracklist').slideToggle();
 });
