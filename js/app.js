b1da691d4a32f3827db38e60b02df312de634378
Julie Ho <julieho.98@gmail.com>
2018-07-31 15:25:38 -0700

    add spotify api

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log(window.location.hash);
+console.log(window.location.hash.substring(1).split('&'));
 
 $('.album-accordion').on('click', '.album', function(event) {
 	$(this).next('.tracklist').slideToggle();
