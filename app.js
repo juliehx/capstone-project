83afd341f45432484c933b554c81eaaadfbe68d9
Julie Ho <julieho.98@gmail.com>
2018-07-31 15:01:56 -0700

    add spotify authorization

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientID = 'c180770a2f4144078103e268866ea767';
-const redirectURI = 'https://juliehx.github.io/songbird/callback';
+const redirectURI = 'https://juliehx.github.io/songbird';
 
-$('.album-accordion').on('click', '.album', function(event) {
-	$(this).next('.tracklist').slideToggle();
-});
+const hash = window.location.hash
+.substring(1)
+.split('&')
+.reduce(function (initial, item) {
+  if (item) {
+    var parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+window.location.hash = '';
 
-window.location.replace(`${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token`);
+let _token = hash.access_token;
+
+if(!_token)
+{
+	window.location.replace(`${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token`);
+}
