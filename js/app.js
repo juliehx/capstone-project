71dd2d088148bfb48e1cb8c4402207921ab87fcd
Julie Ho <julieho.98@gmail.com>
2018-07-31 15:46:28 -0700

    add spotify api

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-console.log(window.location.hash.substring(1).split('&'));
+var queryData = {};
+
+function getQueries(queryObj) {
+	var queries = window.location.hash.substring(1).split('&');
+	for(var q = 0; q < queries.length; ++q) {
+		var pair = queries[q].split('=');
+		queryObj[pair[0]] = decodeURIcomponent(pair[1]);
+	}
+	return queryObj;
+}
 
 $('.album-accordion').on('click', '.album', function(event) {
 	$(this).next('.tracklist').slideToggle();
 });
+
+$(console.log(getQueries(queryData)));
